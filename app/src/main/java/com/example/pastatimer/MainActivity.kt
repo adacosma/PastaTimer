afd2c661a964861bcd9b4a8fd25f5c757a58dc6e
catalinavelea <catalina.ianna@gmail.com>
2025-05-17 20:07:06 +0300

    Stared navigation graph and connected the log in page

--- app/src/main/java/com/example/pastatimer/MainActivity.kt
+++ app/src/main/java/com/example/pastatimer/MainActivity.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.example.pastatimer.navigation.NavGraph
 import com.example.pastatimer.ui.theme.PastaTimerTheme
-import com.example.pastatimer.ui.login.LogInScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PastaTimerTheme {
-                LogInScreen()
+                NavGraph()
             }
         }
     }
